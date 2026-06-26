ef39191b0c538a1ead52a0d0b37aa1843c019480
whoevenisbranch <wenski_109@outlook.com>
2026-06-26 15:24:20 +0100

    refactoring from maven to gradle

--- demo/src/main/java/com/wcpredictor/exceptions/UserExitedException.java
+++
@@ -1,9 +0,0 @@
-package com.wcpredictor.exceptions;
-
-public class UserExitedException extends RuntimeException {
-
-    public UserExitedException(final String message)
-    {
-        super(message);
-    }
-}
